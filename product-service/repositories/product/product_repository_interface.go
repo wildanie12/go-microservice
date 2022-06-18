c95e981f2cb6cd085b0fb7ea24eee6a53ad65bb6
Badar Wildanie <wildanie12@gmail.com>
2022-06-18 14:30:54 +0700

    add: product repository interface

---
+++ product-service/repositories/product/product_repository_interface.go
@@ -0,0 +1,14 @@
+package product
+
+import _domain "github.com/wildanie12/go-microservice/product-service/entities/domain"
+
+type ProductRepositoryInterface interface {
+
+	// FindAll returns product resources in an array of entity struct
+	// based on a given filter and sort query parameter 
+	FindAll(filters []map[string]string) ([]_domain.Product, error)
+
+	// Insert will insert the given product parameter to data source
+	// an error is returned when there was an error
+	Insert(product _domain.Product) (_domain.Product, error)
+}
