8b378c18feae4cc23a74effcf3e343d6988e51b0
Badar Wildanie <wildanie12@gmail.com>
2022-06-18 15:19:08 +0700

    fix: added mongo port

--- product-service/utils/mongo.go
+++ product-service/utils/mongo.go
@@ -12,10 +12,11 @@ import (
 // NewMongoConnection return new connection instance `mongo.Database` to mongodb database
 func NewMongoConnection(config *_config.Config) *mongo.Database {
 	dsn := fmt.Sprintf(
-		"mongodb+srv://%s:%s@%s/?retryWrites=true&w=majority",
+		"mongodb+srv://%s:%s@%s:%s/?retryWrites=true&w=majority",
 		config.Mongo.User,
 		config.Mongo.Pass,
 		config.Mongo.Host,
+		config.Mongo.Port,
 	)
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dsn))
 	if err != nil {
