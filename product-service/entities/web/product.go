67d716adfd8bac8b310683fe539650c68b46cbee
Badar Wildanie <wildanie12@gmail.com>
2022-06-18 15:49:32 +0700

    fix: product entity binding tag

--- product-service/entities/web/product.go
+++ product-service/entities/web/product.go
@@ -3,20 +3,20 @@ package web
 // ProductRequest web entity response provides
 // a schema for product request input
 type ProductRequest struct {
-	Name     string
-	Unit     string
-	Price    int64
-	Category string
-	isActive bool
+	Name     string 	`form:"name" json:"name"`
+	Unit     string 	`form:"unit" json:"unit"`
+	Price    int64 		`form:"price" json:"price"`
+	Category string 	`form:"category" json:"category"`
+	IsActive bool 		`form:"is_active" json:"is_active"`
 }
 
 // ProductResponse web entity 
 // response mapper
 type ProductResponse struct {
-	ID       string
-	Name     string
-	Unit     string
-	Price    int64
-	Category string
-	isActive bool
+	ID       string		`json:"id"`
+	Name     string		`json:"name"`
+	Unit     string		`json:"unit"`
+	Price    int64		`json:"price"`
+	Category string		`json:"category"`
+	IsActive bool		`json:"is_active"`
 }
