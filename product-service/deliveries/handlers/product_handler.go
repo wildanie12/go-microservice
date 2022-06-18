442754f2875311e9e0498d76e765d1add407da72
Badar Wildanie <wildanie12@gmail.com>
2022-06-18 15:06:10 +0700

    docs: product handler notes

--- product-service/deliveries/handlers/product_handler.go
+++ product-service/deliveries/handlers/product_handler.go
@@ -8,17 +8,20 @@ import (
 	_productService "github.com/wildanie12/go-microservice/product-service/services/product"
 )
 
+// ProductHandler main struct
 type ProductHandler struct {
 	productService _productService.ProductServiceInterface
 }
 
+// NewProductHandler act as a constructor 
+// to return new instance of `ProductHandler`
 func NewProductHandler(productService _productService.ProductServiceInterface) *ProductHandler {
 	return &ProductHandler{
 		productService: productService,
 	}
 }
 
-// Index method return 
+// Index serves as list product endpoint controller implementation 
 func (handler ProductHandler) Index(c echo.Context) error {
 	products, err := handler.productService.FindAll([]map[string]string{})
 	if err != nil {
@@ -31,16 +34,12 @@ func (handler ProductHandler) Index(c echo.Context) error {
 	})
 }
 
+// Create responsible to create a product resource endpoint
+// that can be hit using defined request body as a payload data
 func (handler ProductHandler) Create(c echo.Context) error {
 
-	productReq :=_web.ProductRequest {}
+	productReq :=_web.ProductRequest{}
 	c.Bind(&productReq)
-	// productReq := _web.ProductRequest {
-	// 	Name: "Sades Lance Mouse",
-	// 	Unit: "pcs",
-	// 	Price: 28000,
-	// 	Category: "Computer Accessories",
-	// }
 
 	productRes, err := handler.productService.Create(productReq)
 	if err != nil {
